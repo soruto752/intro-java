ac97616139d6492c149cf8a7f66c09ccf69f2a37
soruto752 <sekoniireoluwa@gmail.com>
2026-01-30 17:28:30 +0100

    done task 6

---
+++ js-syntax/index.js
@@ -0,0 +1,41 @@
+//task 1
+//create 3 variables
+
+let a = 8;
+let b = 12;
+let total = a + b;
+console.log("task one");
+console.log(total);
+
+//task 2
+
+let price = 0.5;
+let quantity = 20;
+let cost = price * quantity;
+console.log("task two");
+console.log(cost);
+
+//task 3
+
+const firstname = "Ireoluwa";
+const lastname = "Sekoni";
+const fullname = "Ireoluwa" + " " + "Sekoni";
+console.log("task three");
+console.log(fullname);
+
+//task 4
+
+const score = 25;
+const finalscore = score + 5;
+console.log("task 4");
+console.log(finalscore);
+
+//task 5
+
+//write Javascript code that
+
+let boys = 10;
+const classscore = 98;
+let boystotalscore = boys * classscore;
+console.log("task 5");
+console.log(boystotalscore);
