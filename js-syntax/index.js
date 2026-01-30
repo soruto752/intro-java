c97e3660bcea825709969c0ba9d3f2dca1106ea9
soruto752 <sekoniireoluwa@gmail.com>
2026-01-30 19:31:10 +0100

    done

--- js-syntax/index.js
+++ js-syntax/index.js
@@ -39,3 +39,73 @@ const classscore = 98;
 let boystotalscore = boys * classscore;
 console.log("task 5");
 console.log(boystotalscore);
+
+//task 6
+
+//create two different variables named:
+
+let count = 10;
+let Count = 1000000;
+let result = count + Count;
+console.log("task 6");
+console.log(result);
+
+// task 7
+//Create valid variable names for the following values:
+
+let age = 78;
+let productprice = 15.67;
+let totalstudents = 6;
+console.log("task 7");
+
+//task 8
+//Create two variables x and y .Use arithmetic operators to create:
+
+let x = 67;
+let y = 41;
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+console.log("task 8");
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+
+//task 9
+
+let p = 167;
+let q = 97667;
+let answer = (p + q) * 5;
+console.log("task 9");
+console.log(answer);
+
+//task 11
+// From scratch, write code that:
+let _day = "Friday";
+let $money = 1500;
+let amount67 = 56;
+console.log("task 11");
+
+//task 12
+//From scratch, write code that:
+let studentFirstName = "Tolulope";
+let studentLastName = "Alakiu";
+let totalExamScore = 85;
+console.log("task 12");
+
+//task 13
+
+let word1 = "Java";
+let word2 = "Script";
+let language = word1 + word2;
+console.log("task 13");
+console.log(language);
+
+//task 14
+let height = 8;
+let text = "I am";
+let sentence = text + height;
+console.log("task 14");
+console.log(sentence);
